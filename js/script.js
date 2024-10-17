18806a2a7217f779274afc6f4f59051e9dfaee66
jeevareka296@gmail.com <jeevareka296@gmail.com>
2024-10-17 05:33:05 +0530

    animation,links added

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ hamburger.addEventListener('click', () => {
     navList.classList.toggle('active'); // Toggle 'active' class
     prod.classList.toggle('active');
   });
+
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+        {pageLanguage: 'en'},
+        'google_translate_element'
+    );
+} 
+
+const container = document.querySelector('.animebg');
+
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY-1200; 
+  container.style.transform = `translateX(-${scrollPosition*0.5}px)`
+});
